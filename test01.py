c221887b454e8ba4af1225f1c0ccbdf200fa94e7
sjhyan <aaa@abc.com>
2026-03-25 10:46:14 +0900

    Second commit

--- test01.py
+++ test01.py
@@ -3,6 +3,7 @@ def addNum(a,b):
     sum = a +b
     return sum
     
-  num01 = 10
-  num02 = 20
-  print(addNum(num01,num02))
+num01 = 10
+num02 = 20
+print(addNum(num01,num02))
+print("program End!")
